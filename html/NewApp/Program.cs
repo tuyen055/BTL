66b303087b41d0d986288350c7d03d646b5fc535
tuyen055 <142509972+tuyen055@users.noreply.github.com>
2023-09-07 18:02:01 +0700

    upcode

--- html/NewApp/Program.cs
+++ html/NewApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Pogram{
+
+        private static void Main(string[] args){
+    int a;
+    System.Console.Write("Gia tri cua a la: ");
+    a=Convert.ToInt32(Console.ReadLine());
+    int b;
+    System.Console.Write("Gia tri cua b la: ");
+    b=Convert.ToInt32(Console.ReadLine());
+    int c=a-b;
+    int d=a*b;
+    int e=a/b;
+    int f=a%b;
+    System.Console.WriteLine("Gia tri cua c= " +c);
+     System.Console.WriteLine("Gia tri cua d= " +d);
+     System.Console.WriteLine("Gia tri cua e= " +e);
+     System.Console.WriteLine("Gia tri cua f= " +f);
+     }
+  }
