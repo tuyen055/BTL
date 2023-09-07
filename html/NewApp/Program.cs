c2c469ce3e6cd9d250cc66eee6bcf5f82938f865
tuyen055 <142509972+tuyen055@users.noreply.github.com>
2023-09-07 18:15:07 +0700

    upcode

--- html/NewApp/Program.cs
+++ html/NewApp/Program.cs
@@ -1,19 +1,10 @@
-﻿internal class Pogram{
-
-        private static void Main(string[] args){
-    int a;
-    System.Console.Write("Gia tri cua a la: ");
-    a=Convert.ToInt32(Console.ReadLine());
-    int b;
-    System.Console.Write("Gia tri cua b la: ");
-    b=Convert.ToInt32(Console.ReadLine());
-     if (a>b){
-            System.Console.Write("a lon hon b ");
-
-        }else if(a<b){
-            System.Console.Write("a nho hon b");
-        }else{
-            System.Console.Write("a bang b ");
-        }
-     }
-  }
+﻿﻿internal class Program{
+    private static void Main(String[] args){
+        const int a=10;
+        int b;
+        System.Console.Write("gia tri cua b la: ");
+        b=Convert.ToInt32(Console.ReadLine());
+        int c=a+b;
+        System.Console.WriteLine("gia tri cua c= " +c);
+    }
+}
