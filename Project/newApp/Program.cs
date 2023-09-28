5071aa39c1d923ca5565d52c7285ed159bb4e98c
tuyen055 <142509972+tuyen055@users.noreply.github.com>
2023-09-28 17:09:15 +0700

    Array

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -1,4 +1,16 @@
 ﻿using newApp.model;
-Employee emp = new Employee();
-emp.Nhap();
-emp.Xuat();
+int n;
+System.Console.Write(" n = ");
+n = Convert.ToInt16(Console.ReadLine());
+int[] intArr =  new int[n];
+
+for(int i = 0; i < intArr.Length; i++)
+{
+   intArr[0] = 4;
+   intArr[i] = i + 2;
+
+
+}
+for (int i = 0; i < intArr.Length; i++){
+    System.Console.WriteLine("Mang[" + i + "] = " + intArr[i]);
+}
